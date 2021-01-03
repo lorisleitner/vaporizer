473ccc31cf0531104e7856b7ea3a9738247e45f4
Loris Leitner <lorisleitner@live.com>
2021-01-04 00:13:43 +0100

    Vaporize song

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const {exec} = require("child_process");
 
 const app = express();
+const storage = multer.diskStorage({
+    destination: "tmp/",
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({storage: storage});
 
 app.use(express.static("public"));
 
-app.post("/vaporize", (req, res) => {
-    //res.download(req.files["file"].tempFilePath, req.files["file"].name);
-});
+app.post("/vaporize", upload.single("file"),
+    (req, res, next) => {
+        const file = req.file;
+        if (!file) {
+            return res.sendStatus(400);
+        }
+
+        const outPath = `tmp/${path.basename(file.filename, path.extname(file.filename))}-vaporized.mp3`;
+        exec(`sox "${file.path}" "${outPath}" speed 0.7 bass +1 treble -10`, (error, stdout, stderr) => {
+            fs.unlink(file.path, (err) => {
+            });
+
+            if (error) {
+                console.log("sox error: " + error);
+                return res.sendStatus(400);
+            }
+
+            const downloadName = path.basename(file.originalname, path.extname(file.originalname))+ " Vaporized.mp3";
+            return res.download(outPath, downloadName, (err) => {
+                fs.unlink(outPath, (err) => {});
+            });
+        });
+
+    });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
